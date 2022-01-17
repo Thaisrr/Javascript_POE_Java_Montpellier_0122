44212a7097350b4250f98d574e08096a376a6b75
Thais Revillon <thais.r@live.fr>
2022-01-17 15:01:53 +0100

    :sparkles: tests valeurs

--- Presentation/assets/js/hello2.js
+++ Presentation/assets/js/hello2.js
@@ -1,1 +1,7 @@
 sum(10, 20);
+
+Math.random();
+// retourne un nombre compris entre 0 ( inclu ) et 1 ( non inclu )
+
+const random = Math.floor( Math.random() * 100 ) + 1;
+console.log('random : ' + random)
