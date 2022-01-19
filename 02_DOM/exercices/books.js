6ea8e1393373d5ae9bfb6531814632cc766e07f2
Thais Revillon <thais.r@live.fr>
2022-01-19 16:36:43 +0100

    :sparkles: Exercice DOM books v4 et V5

--- 02_DOM/exercices/books.js
+++ 02_DOM/exercices/books.js
@@ -1,9 +1,13 @@
 const books = ['Les Hauts de Hurlevent', 'La Passe Miroir', 'Frankenstein'];
 const obj_books = [
-    {title: 'Les Hauts de Hurlevent', author: 'Emily Brontë', read: false},
-    {title: 'La Passe Miroir', author: 'Christelle Dabos', read: true },
-    {title: 'Frankenstein', author: 'Mary Shelley', read: true},
-    {title: 'Nana', author: 'Emile Zola', read: false}
+    {title: 'Les Hauts de Hurlevent', author: 'Emily Brontë', read: false,
+        img: 'https://www.ecoledesloisirs.fr/sites/default/files/couverture/couvhhurlevent.jpg'},
+    {title: 'La Passe Miroir', author: 'Christelle Dabos', read: true,
+        img: 'https://lislysworld.fr/wp-content/uploads/2019/05/La-Passe-Miroir-tome-1-les-fianc%C3%A9s-de-l%E2%80%99hiver-1140x668.png'},
+    {title: 'Frankenstein', author: 'Mary Shelley', read: true,
+        img: "https://static.fnac-static.com/multimedia/Images/FR/NR/1d/e9/1d/1960221/1507-1/tsp20200429070322/Frankenstein-de-Mary-Shelley.jpg"},
+    {title: 'Nana', author: 'Emile Zola', read: false,
+        img: 'https://images.radio-canada.ca/q_auto,w_960/v1/ici-premiere/16x9/nana-zola-edouard-manet-cocotte.jpg'}
 ];
 
 // Affiche des livres depuis une liste de string
@@ -45,8 +49,59 @@ function displayBooks2() {
     }
 }
 
+function displayBooksCards() {
+    const deck = document.querySelector('#deck');
+    for (let b of obj_books) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const header = document.createElement("div");
+        header.classList.add("card-img");
+
+        const img = document.createElement('img');
+        img.src = b.img;
+        img.alt = 'Couverture du livre ' + b.title;
+
+        header.appendChild(img);
+        card.appendChild(header);
+
+        const body = document.createElement('div');
+        body.classList.add('card-body');
+
+        const title = document.createElement('h3');
+        title.textContent = b.title;
+        if(b.read) {
+            title.classList.add('read');
+        }
+
+        const author = document.createElement('p');
+        author.textContent = '-- par ' + b.author;
+
+        const btn = document.createElement('button');
+        btn.textContent = getButtonText(b.read);
+        btn.addEventListener('click', () => {
+           b.read = !b.read;
+           btn.textContent = getButtonText(b.read);
+           title.classList.toggle('read');
+           // pas besoin de refaire des requêtes pour récupérer les éléments HTML
+        });
+
+        body.appendChild(title);
+        body.appendChild(author);
+        body.appendChild(btn);
+
+        card.appendChild(body);
+        deck.appendChild(card);
+    }
+}
+
+const getButtonText = (read) => (read)? 'Déjà lu !' : 'Par encore lu';
+
 
 document.addEventListener('DOMContentLoaded', function () {
     displayBooks1();
     displayBooks2();
+    displayBooksCards();
 })
+
+
